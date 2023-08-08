bb6a97a912389af77cc7fa1cd503b51ce80724a9
gaoneee <mmm3226@naver.com>
2023-08-08 16:40:52 +0900

    클래스연습 완료

--- 상속01.py
+++ 상속01.py
@@ -7,16 +7,20 @@ class Person:
         print("Info(Name:{0}, Phone Number: {1})".format(self.name, self.phoneNumber))
 
 class Student(Person):
+    #메서드 재정의(덮어쓰기)
     def __init__(self, name, phoneNumber, subject, studentID):
-        self.name = name
-        self.phoneNumber = phoneNumber
+        #부모 클래스의 초기화 메서드 부르면 됨
+        Person.__init__(self,name,phoneNumber)
         self.subject = subject
         self.studentID = studentID
-
+    def printInfo(self):
+        print("Subject: {0},StudentID: {1})".format(self.subject, self.studentID))
 
 p = Person("전우치", "010-222-1234")
 s = Student("이순신", "010-111-1234", "컴공", "991122")
 print(p.__dict__)
 print(s.__dict__)
+s.printInfo()
+p.printInfo()
 
 
